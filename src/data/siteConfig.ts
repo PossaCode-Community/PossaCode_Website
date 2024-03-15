c7975fb8817f9f4e4211c8e979522f13e6b50b42
brx-nbif <blondeau.nbif@mail.ru>
2024-03-15 23:08:32 +0100

    [START]: PossaCode official Website lunch

---
+++ src/data/siteConfig.ts
@@ -0,0 +1,10 @@
+export const MetaData: {
+  title: string;
+  description: string;
+  ogImage: string;
+} = {
+  title: "PossaCode Community",
+  description:
+    "PossaCode is a community of developers who share their knowledge and experience. We are here to help each other. Join us and let's build something great together!",
+  ogImage: "/images/og.png",
+};
