a26e787c41a73bf50d84cf29661071c28c3552c6
brx-nbif <blondeau.nbif@mail.ru>
2024-03-22 20:15:33 +0100

    [UPDATE]: aupdating the astro db

--- .netlify/functions-internal/ssr/chunks/_.._UrcmZKnz.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/__B3_Vygd6.mjs').then(n => n._);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/contact_Bkt3b_Xg.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/contact_D4RIET7r.mjs');
-
-export { page };
