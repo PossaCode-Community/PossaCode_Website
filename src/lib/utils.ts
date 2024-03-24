0790d0bf7e2223c4f08d3895f67ab951324bf6b3
brx-nbif <blondeau.nbif@mail.ru>
2024-03-24 02:50:48 +0100

    [ADD]: ThemeSwitcher bug fixed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getGithubUserName(url: string) {
+  return url.replace(/^https:\/\/github\.com\/|\/$/g, '')
 }
