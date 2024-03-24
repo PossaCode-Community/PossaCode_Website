72e601e8aa8ef10da3c708e427fc362587f4d2f2
brx-nbif <blondeau.nbif@mail.ru>
2024-03-24 19:29:07 +0100

    [ADD]: Adding sharpImageService because image optimization is supported in Netlify as Adapter

--- .netlify/functions-internal/ssr/chunks/index_BSe7xdQp.mjs
+++
@@ -1,6 +0,0 @@
-;!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="e5990e61-599b-4cb0-9bdb-d52d0333ee35",e._sentryDebugIdIdentifier="sentry-dbid-e5990e61-599b-4cb0-9bdb-d52d0333ee35")}catch(e){}}();export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_BSuuOfYM.mjs').then(n => n.a);
-
-export { page };
-//# sourceMappingURL=index_BSe7xdQp.mjs.map
